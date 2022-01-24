45ad98de9bd7656a82f7364e21eebfd000d2feb6
Vladimir Kolesnikov <9858713@gmail.com>
2022-01-24 14:24:17 +0200

    refactor task3.js add named groups in regexp

--- task3.js
+++ task3.js
@@ -8,11 +8,13 @@ const changeLink = (link) => {
 }
 
 const parse = (mdLink) => {
-    const textUrlTilleTemplate = /\[(.+)\]\(([^"]+)( "(.+)")?\)/; // match groups: 1-text, 2-url, 4-title
+    const regexp = /\[(?<textGroup>.+)\]\((?<urlGroup>[^"]+)( "(?<titleGroup>.+)")?\)/;
+    const matchGroups = mdLink.match(regexp).groups;
+
     return ({
-        text: mdLink.match(textUrlTilleTemplate)[1],
-        href: mdLink.match(textUrlTilleTemplate)[2],
-        title: mdLink.match(textUrlTilleTemplate)[4],
+        text: matchGroups.textGroup,
+        href: matchGroups.urlGroup,
+        title: matchGroups.titleGroup,
         renderAsHtml() {
             const titleAttribute = this.title ? ` title="${this.title}"` : '';
             return `<a href="${this.href}"${titleAttribute} >${this.text}</a>`;
